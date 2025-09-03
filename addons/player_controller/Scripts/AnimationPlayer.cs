32aee18e4cf25a6a1e05f3824a0b04291deb2e13
Than <thanthemannn@gmail.com>
2025-09-03 17:59:57 -0400

    changed character controllers, added phantom camera

--- addons/player_controller/Scripts/AnimationPlayer.cs
+++
@@ -1,9 +0,0 @@
-using PolarBears.PlayerControllerAddon;
-
-public partial class AnimationPlayer : Godot.AnimationPlayer
-{
-	public void PlayCameraRotationOnDeath()
-	{
-		Play(Constants.PLAYERS_HEAD_ANIMATION_ON_DYING);
-	}
-}
