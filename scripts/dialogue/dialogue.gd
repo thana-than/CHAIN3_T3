193e1181ad6b4f82e66fa192c0247795d1e64c31
Hamada <11mhg@queensu.ca>
2025-09-05 00:20:52 -0400

    feat: Adding example dialogue + style changes to scene_start

--- scripts/dialogue/dialogue.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("_debug_test_dialogue"):
-		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/test_001.dialogue"), "start")
-		return
