92148484fb0dc6635eee0d4e505a8a59bc306f0e
Than <thanthemannn@gmail.com>
2025-09-08 14:49:54 -0400

    chore: HUD signal code

---
+++ scripts/UI/HUD.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var crosshair : InteractionCrosshair = get_node(NodePath("Crosshair"))
+
+func _interactor_on_focus(Interactable3D: Variant) -> void:
+	crosshair.update_crosshair(Interaction.Type.INTERACT)
+
+func _interactor_on_unfocus(Interactable3D: Variant) -> void:
+	crosshair.update_crosshair(Interaction.Type.NONE)
