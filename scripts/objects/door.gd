8652442595840845b6438ad0491ffcbf2853406d
Than <thanthemannn@gmail.com>
2025-09-08 19:33:11 -0400

    feat: door script

---
+++ scripts/objects/door.gd
@@ -0,0 +1,103 @@
+extends CollisionObject3D
+class_name Door
+
+enum AnimType { SLIDE, HINGE }
+
+signal on_open
+signal on_close
+signal on_locked
+signal on_unlocked
+signal on_locked_attempt
+
+## Remains open for specified amount of seconds. Will remain open indefinitely if set to 0.0.
+@export_range(0.0, 60.0) var remain_open_time := 0.0
+@export var door_mesh_node : Node3D
+
+@export_group("Default State")
+@export var start_open := false
+@export var start_locked := false
+
+@export_group("Animation")
+@export var anim_type := AnimType.SLIDE
+@export var anim_open_time := .5
+@export var anim_close_time := .5
+
+var _locked = false
+
+@onready var animation : AnimationPlayer = get_node("AnimationPlayer")
+@onready var collider : CollisionShape3D = get_node("CollisionShape3D")
+@onready var interactable : Interactable3D = get_node("Interactable3D")
+@onready var timer : Timer = get_node("Timer")
+@onready var mesh_parent : Node3D = get_node("root_transform/hinge/pivot") 
+@onready var logger = Logger.new("door.gd: " + name)
+
+func _ready():
+	if start_locked:
+		lock()
+	else:
+		unlock()
+		
+	if start_open:
+		open()
+	else:
+		close()
+		
+	interactable.interacted.connect(toggle_open)
+	timer.timeout.connect(close)
+	if door_mesh_node:
+		door_mesh_node.reparent(mesh_parent, false)
+	else:
+		logger.err("Missing door_mesh_node!")
+		
+func _get_anim_name(_anim_type: AnimType):
+	return "anim_door_" + AnimType.keys()[_anim_type].to_lower()
+
+func is_open() -> bool:
+	return collider.disabled
+
+func is_locked() -> bool:
+	return _locked
+
+func lock():
+	if is_locked():
+		return
+	if is_open():
+		close()
+	on_locked.emit()
+	_locked = true
+
+func unlock():
+	if not is_locked():
+		return
+	on_unlocked.emit()
+	_locked = false
+	
+func toggle_open():
+	if is_open():
+		close()
+	else:
+		open()
+
+func open():
+	if is_open():
+		return
+	if is_locked():
+		on_locked_attempt.emit()
+		return
+	
+	var speed = 1.0 / anim_open_time if anim_open_time > 0 else 1000.0
+	animation.play(_get_anim_name(anim_type), -1, speed)
+	collider.disabled = true
+	on_open.emit()
+	if remain_open_time > 0.0:
+		timer.start(remain_open_time)
+
+func close():
+	if not is_open():
+		return
+		
+	var speed = 1.0 / anim_open_time if anim_open_time > 0 else 1000.0
+	animation.play(_get_anim_name(anim_type), -1, -speed, true)
+	collider.disabled = false
+	on_close.emit()
+	timer.stop()
