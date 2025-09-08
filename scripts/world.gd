687f9336a4b7bb35f476941759a2740b549022bb
Than <thanthemannn@gmail.com>
2025-09-07 22:41:59 -0400

    feat: world script including debug config loading and usage

---
+++ scripts/world.gd
@@ -0,0 +1,36 @@
+extends Node
+
+@onready var player: Player = get_node(NodePath("Player"))
+@onready var room_manager : = get_node(NodePath("Train"))
+
+var logger := Logger.new("world")
+
+#TODO config option to set / change chain flags?
+var debug_config : DebugConfig
+var debug_config_path := "res://local/debug-config.tres"
+var debug_room_spawn_path := "door_enter"
+
+func _ready() -> void:
+	if OS.is_debug_build():
+		load_debug_config()
+		if debug_config:
+			try_move_player_to_room_id(debug_config.spawn_room_id)
+
+func load_debug_config():
+	if ResourceLoader.exists(debug_config_path):
+		debug_config = load(debug_config_path)
+		
+func try_move_player_to_room_id(room_id: String):
+	for room in room_manager.rooms:
+		if room.name == room_id:
+			var room_transform = room.get_node(NodePath(debug_room_spawn_path)) as Node3D
+			if room_transform:
+				room.enter()
+				player.global_position = room_transform.global_position + Vector3.UP
+				#TODO rotation mapping
+				#player.global_rotation = -room_transform.global_rotation
+				player.translate(Vector3.FORWARD)
+				logger.log("Moved player to room " + room.name)
+			else:
+				logger.log("Failed to move player to room " + room.name + ". Room requires Node3D named: " + debug_room_spawn_path)
+			break
