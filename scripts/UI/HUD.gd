3dc86b3c4660a468a9bf9d8ea5298a87508a5943
Than <thanthemannn@gmail.com>
2025-09-08 20:47:57 -0400

    fix: underscore for HUD signals to disable warning

--- scripts/UI/HUD.gd
+++ scripts/UI/HUD.gd
@@ -2,8 +2,8 @@ extends Control
 
 @onready var crosshair : InteractionCrosshair = get_node(NodePath("Crosshair"))
 
-func _interactor_on_focus(Interactable3D: Variant) -> void:
+func _interactor_on_focus(_interactable: Interactable3D) -> void:
 	crosshair.update_crosshair(Interaction.Type.INTERACT)
 
-func _interactor_on_unfocus(Interactable3D: Variant) -> void:
+func _interactor_on_unfocus(_interactable: Interactable3D) -> void:
 	crosshair.update_crosshair(Interaction.Type.NONE)
