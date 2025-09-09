b7eac7916c01a1276b579b673fb70231399e94a3
Than <thanthemannn@gmail.com>
2025-09-09 08:09:16 -0400

    feat: culling now polls every few units of movement. Tracks the distance between the camera view and each room

--- scripts/room_manager.gd
+++ scripts/room_manager.gd
@@ -1,10 +1,11 @@
 extends Node
 class_name RoomManager
 
-@export var surrounded_room_culling := 1
-
 var active_room: Room
 var rooms: Array[Room]
+
+var last_cull_position : Vector3
+@export var cull_distance_delta := 5.0 
 
 func _enter_tree() -> void:
 	rooms = []
@@ -15,8 +16,11 @@ func _enter_tree() -> void:
 			child.room_index = index
 			bind_room(child)
 			index += 1
-
-	room_culling()
+	
+func _process(_delta):
+	var pos = _get_cull_position()
+	if pos.distance_squared_to(last_cull_position) > cull_distance_delta * cull_distance_delta:
+		room_culling(pos)
 
 func bind_room(room: Room) -> void:
 	room.entered.connect(_on_room_entered.bind(room))
@@ -27,15 +31,21 @@ func _on_room_entered(room) -> void:
 
 	active_room = room
 	room_culling()
-
-func room_culling() -> void:
-	if active_room == null:
+	
+func _get_cull_position():
+	return get_viewport().get_camera_3d().global_position
+	
+func activate_room(room: Room, activate: bool = true):
+	if room.is_inside_tree() == activate:
 		return
-	for i in range(len(rooms)):
-		var distance = abs(i - active_room.room_index)
-		if distance <= surrounded_room_culling:
-			if !rooms[i].is_inside_tree():
-				add_child(rooms[i])
-		else:
-			if rooms[i].is_inside_tree():
-				remove_child(rooms[i])
+	if activate:
+		add_child(room)
+	else:
+		remove_child(room)
+
+func room_culling(position : Vector3 = _get_cull_position()) -> void:
+	for room in rooms:
+		var distance = room.cull_position.distance_squared_to(position)
+		var within_distance = distance <= room.cull_distance * room.cull_distance
+		activate_room(room, within_distance or room == active_room)
+	last_cull_position = position
