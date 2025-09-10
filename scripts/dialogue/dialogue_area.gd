af7d053983cde206443371129e26ad464259c2bd
Than <thanthemannn@gmail.com>
2025-09-09 21:39:15 -0400

    feat: dialogue revamp

--- scripts/dialogue/dialogue_area.gd
+++ scripts/dialogue/dialogue_area.gd
@@ -3,27 +3,18 @@
 #shape 3d is triggered.
 extends Area3D
 
-@export var custom_balloon_resource: PackedScene = preload("res://assets/dialogue/styles/gothic_silent_hill/Balloon.tscn")
-@export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "start"
-@export var is_repeatable: bool = false
+@export var dialogue_settings := DialogueSettings.new()
+@export var player_settings := DialoguePlayerSettings.new()
 @export var debug_verbose: bool = false
 
-var dialogue_player: DialoguePlayer = DialoguePlayer.new()
-var logger: Logger = null
+@onready var dialogue_player: DialoguePlayer = DialoguePlayer.new(dialogue_settings, player_settings)
+@onready var logger := Logger.new(name)
 
 func _ready() -> void:
-	logger = Logger.new(self.name)
-	setup_dialogue_player()
+	add_child(dialogue_player)
 	self.body_entered.connect(_on_body_entered)
 	if debug_verbose:
 		logger.log("_on_body_entered has been connected to the body_entered signal.")
-
-func setup_dialogue_player() -> void:
-	dialogue_player.custom_balloon_resource = custom_balloon_resource
-	dialogue_player.dialogue_resource = dialogue_resource
-	dialogue_player.dialogue_start = dialogue_start
-	dialogue_player.is_repeatable = is_repeatable
 
 # When triggered run dialogue unless this is not a repeatable trigger and 
 # it has already triggered it once.
