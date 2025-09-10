7180cbe1c0dfec38f0c66e4bd25d4e5e5f9fb9bd
Hamada <11mhg@queensu.ca>
2025-09-10 17:00:19 -0400

    feat: add audio_event_resource

--- scripts/audio_emitters/footstep_emitter.gd
+++ scripts/audio_emitters/footstep_emitter.gd
@@ -1,12 +1,11 @@
 extends Node3D
 
-@export var volume: float = 2.0
+@export var footstep_event: AudioEventResource = preload("res://assets/resources/audio_events/SFX_Player_Footsteps.tres")
 
 @onready var _logger := Logger.new(name)
 
 var _event: FmodEvent
 var controller: Player
-var footstep_event: String = "event:/SFX/Footsteps"
 
 func _ready() -> void:
 	_setup_fmod_event()
@@ -15,9 +14,8 @@ func _ready() -> void:
 		controller.stepped.connect(_on_stepped)
 
 func _setup_fmod_event() -> void:
-	_event = FmodServer.create_event_instance(footstep_event)
-	_event.set_3d_attributes(controller.global_transform)
-	_event.volume = volume
+	_event = FmodServer.create_event_instance(footstep_event.name)
+	_event.set_3d_attributes(self.global_transform)
 	_event.stop(FmodServer.FMOD_STUDIO_STOP_IMMEDIATE)
 
 func _on_stepped() -> void:
@@ -25,6 +23,4 @@ func _on_stepped() -> void:
 		return
 	if (_event.get_playback_state() == FmodEvent.FMOD_STUDIO_PLAYBACK_PLAYING):
 		_event.stop(FmodServer.FMOD_STUDIO_STOP_IMMEDIATE)
-	
 	_event.start()
-	_logger.log("_on_stepped called - played one shot")
