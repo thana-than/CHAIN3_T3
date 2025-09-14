21b0eb5c5bb99f01aad28747b86e73cf5df1bd6a
Than <thanthemannn@gmail.com>
2025-09-14 14:07:26 -0400

    dialogue_audio changes

--- scripts/dialogue/dialogue_audio.gd
+++ scripts/dialogue/dialogue_audio.gd
@@ -5,16 +5,18 @@ var _audio_preload : AudioEventResource = preload("res://assets/resources/audio_
 
 var dialogue_settings : DialogueSettings
 var dialogue_player : DialoguePlayer
+var dialogue_audio : AudioEventResource
 
 var stop_timer : Timer
 
-func _init(_dialogue_settings, _dialogue_player):
+func _init(_dialogue_settings, _dialogue_player, _dialogue_audio = _audio_preload):
 	dialogue_settings = _dialogue_settings
 	dialogue_player = _dialogue_player
+	dialogue_audio = _dialogue_audio
 	
 func _ready() -> void:
 	if not audio_event_resource:
-		audio_event_resource = _audio_preload
+		audio_event_resource = dialogue_audio
 		
 	stop_timer = Timer.new()
 	stop_timer.wait_time = dialogue_settings.audio_stop_delay
--- scripts/dialogue/dialogue_player.gd
+++ scripts/dialogue/dialogue_player.gd
@@ -8,7 +8,7 @@ signal on_line_end()
 
 @export var settings := DialogueSettings.new()
 @export var player_settings := DialoguePlayerSettings.new()
-var timer := Timer.new()
+var end_dialogue_timer := Timer.new()
 var _shown: bool = false
 var current_balloon
 var dialogue_state := {
@@ -20,9 +20,9 @@ func _init(dialogue_settings := settings, dialogue_player_settings := player_set
 	player_settings = dialogue_player_settings
 	
 func _ready():
-	add_child(timer)
-	timer.one_shot = true
-	timer.timeout.connect(cycle_next)
+	add_child(end_dialogue_timer)
+	end_dialogue_timer.one_shot = true
+	end_dialogue_timer.timeout.connect(end_dialogue)
 
 func play_dialogue() -> void:
 	if current_balloon:
@@ -38,20 +38,27 @@ func play_dialogue() -> void:
 		_shown = true
 	
 	current_balloon = DialogueManager.show_dialogue(settings, [dialogue_state.duplicate(true)]);
-	current_balloon.OnNext.connect(func(_line): restart_timer())
-	restart_timer()
+	end_dialogue_timer.stop()
 	on_play_dialogue.emit()
 	dialogue_state.interaction_index += 1
 	await current_balloon.ready
 	current_balloon.dialogueLabel.started_typing.connect(on_line_start.emit)
-	current_balloon.dialogueLabel.finished_typing.connect(on_line_end.emit)
+	current_balloon.dialogueLabel.finished_typing.connect(line_ended)
 
-func restart_timer():
-	if player_settings.next_after_seconds > 0:
-		timer.start(player_settings.next_after_seconds)
+func restart_end_dialogue_timer():
+	if player_settings.stop_after_seconds > 0:
+		end_dialogue_timer.start(player_settings.stop_after_seconds)
 
 func cycle_next():
 	if not current_balloon:
 		return
 	current_balloon.Next()
 	on_next_dialogue.emit()
+	
+func end_dialogue():
+	if current_balloon:
+		DialogueManager._clear_last_dialogue()
+	
+func line_ended():
+	restart_end_dialogue_timer()
+	on_line_end.emit()
